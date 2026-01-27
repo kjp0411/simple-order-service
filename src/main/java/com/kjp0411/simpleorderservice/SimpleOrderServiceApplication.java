174aa8c42c48a985e2e4a6cc17336246d8e1048b
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2026-01-27 15:25:28 +0900

    chore: initialize Spring Boot project with JPA and H2

---
+++ src/main/java/com/kjp0411/simpleorderservice/SimpleOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kjp0411.simpleorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjp0411/simpleorderservice/SimpleOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjp0411.simpleorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
